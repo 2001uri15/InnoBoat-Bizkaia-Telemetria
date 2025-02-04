3a5b946a6c10d7f87bbaaa4322974d56113dd1ae
2001uri15 <2001uri15@gmail.com>
2025-02-04 15:10:27 +0100

    GPS funcionando

--- electronica/gps/gps.ino
+++ electronica/gps/gps.ino
@@ -1,36 +1,46 @@
 #include <TinyGPS++.h>
 
-// Instancia de TinyGPS++
 TinyGPSPlus gps;
 
 void setup() {
-  Serial.begin(9600);       // Monitor Serie
-  Serial1.begin(9600);      // Comunicación con el GPS (pines 18 y 19)
+  Serial.begin(9600);
+  Serial1.begin(9600); // GPS en pines 18 (TX) y 19 (RX)
   Serial.println("Esperando datos del GPS...");
 }
 
 void loop() {
   while (Serial1.available() > 0) {
     char c = Serial1.read();
-    Serial.write(c); // Imprime datos NMEA crudos
-
-    // Procesar datos con TinyGPS++
     gps.encode(c);
 
-    if (gps.location.isUpdated()) {
-      Serial.println("\n--- Datos GPS ---");
-      Serial.print("Latitud: "); Serial.println(gps.location.lat(), 6);
-      Serial.print("Longitud: "); Serial.println(gps.location.lng(), 6);
-      Serial.print("Altitud: "); Serial.println(gps.altitude.meters());
-      Serial.print("Satélites: "); Serial.println(gps.satellites.value());
-      Serial.print("Velocidad (km/h): "); Serial.println(gps.speed.kmph());
-      Serial.print("Fecha: "); Serial.print(gps.date.day());
-      Serial.print("/"); Serial.print(gps.date.month());
-      Serial.print("/"); Serial.println(gps.date.year());
-      Serial.print("Hora (UTC): "); Serial.print(gps.time.hour());
-      Serial.print(":"); Serial.print(gps.time.minute());
-      Serial.print(":"); Serial.println(gps.time.second());
-      Serial.println("-----------------\n");
+    if (gps.satellites.isUpdated() || gps.location.isUpdated()) {
+      Serial.println("\n--- Información GPS ---");
+      
+      // Satélites en uso para el fix
+      Serial.print("Satélites conectados: ");
+      Serial.println(gps.satellites.value());
+
+      // Satélites visibles (se actualiza con GSV)
+      Serial.print("Satélites visibles: ");
+      if (gps.satellites.isValid()) {
+        Serial.println(gps.satellites.value()); // Mismo valor si GSV no está disponible
+      } else {
+        Serial.println("No disponible");
+      }
+
+      // Coordenadas
+      if (gps.location.isValid()) {
+        Serial.print("Latitud: ");
+        Serial.println(gps.location.lat(), 6);
+        
+        Serial.print("Longitud: ");
+        Serial.println(gps.location.lng(), 6);
+      } else {
+        Serial.println("Latitud: No disponible");
+        Serial.println("Longitud: No disponible");
+      }
+
+      Serial.println("-------------------------");
     }
   }
 }
